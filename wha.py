a868ba902c11b9a2aff91aabf23265a7d7dd898e
rubioelastic <45874474+rubioelastic@users.noreply.github.com>
2022-07-27 01:35:28 +0200

    Add files via upload

---
+++ wha.py
@@ -0,0 +1,19 @@
+
+import pywhatkit
+
+pywhatkit.sendwhatmsg('+34684240643','Python Watsapp Bot, mensage de prueba',1,25)
+
+
+
+# Send message to a contact
+#phone_number = input("Enter phone number: ")
+
+#pywhatkit.sendwhatmsg(phone_number, "Test", 7, 21)
+#pywhatkit.sendwhatmsg(phone_number, "Test", 7, 25, 15, True, 2)
+
+
+
+# Send message to a group
+#group_id = input("Enter group id: ")
+
+#pywhatkit.sendwhatmsg_to_group(group_id, "Test Group", 7, 31)
